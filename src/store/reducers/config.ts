9f265d3b15ea9f846be99266508f166128aebe46
winter2048 <522112669@qq.com>
2025-01-21 21:13:46 +0800

    update

--- src/store/reducers/config.ts
+++ src/store/reducers/config.ts
@@ -15,9 +15,11 @@ const configSlice = createSlice({
       state.THEME = actions.payload.THEME;
     },
     initState: (state, actions) => {
-      const { SERVER_URL, APP_TITLE, ICON_URL, GITHUB_URL } =
-        actions.payload;
+      const { SERVER_URL, APP_TITLE, ICON_URL, GITHUB_URL } = actions.payload;
       state.SERVER_URL = SERVER_URL;
+      if (window.location.hostname.endsWith(".cn")) {
+        state.SERVER_URL = SERVER_URL.replace(".com", ".cn");
+      }
       state.APP_TITLE = APP_TITLE;
       state.ICON_URL = ICON_URL;
       state.GITHUB_URL = GITHUB_URL;
